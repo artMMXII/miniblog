51d60e25c40984c4fa07798db4792c0340b8d40b
Arthur Khasiev <hasiev_artur@mail.ru>
2022-11-18 18:48:20 +0700

    create and install model Post

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Post(models.Model):
+    '''данные о посте'''
+    title = models.CharField('Заголовок записи', max_length=100)
+    discription = models.TextField('Текст записи')
+    author = models.CharField('Имя автора', max_length=100)
+    date = models.DateField('Дата публикации')
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+
+
 
